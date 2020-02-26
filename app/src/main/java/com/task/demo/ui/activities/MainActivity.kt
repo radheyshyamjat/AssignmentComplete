af4feb5d85694ef2e7a9754c0fb30fbc56b366a4
radheyshyamjat <radheyit@gmail.com>
2020-02-26 15:09:54 +0530

    All http IP Whitlist

--- app/src/main/java/com/task/demo/ui/activities/MainActivity.kt
+++ app/src/main/java/com/task/demo/ui/activities/MainActivity.kt
@@ -1,34 +1,18 @@
 package com.task.demo.ui.activities
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
 import com.task.demo.R
-import com.task.demo.ui.fragments.FilterFragment
 import com.task.demo.ui.fragments.NobelPrizeFragment
-const val EXTRAS_LOAD_FRAGMENT = "fragment_load"
 
 class MainActivity : AppCompatActivity() {
-    companion object {
-        fun navigateToCommonActivity(appCompact: AppCompatActivity, item: Int) {
-            val intent = Intent(appCompact, MainActivity::class.java)
-            intent.putExtra(EXTRAS_LOAD_FRAGMENT, item)
-            appCompact.startActivity(intent)
-        }
-    }
 
-    val list: MutableList<String> = ArrayList()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         title = "Prizes"
-
         loadFragmentWithoutBack(NobelPrizeFragment())
     }
 
@@ -43,26 +27,6 @@ class MainActivity : AppCompatActivity() {
         val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()
         transaction.replace(R.id.fragment_container, fragment, fragment.javaClass.simpleName)
         transaction.commit()
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        val inflater = menuInflater
-        inflater.inflate(R.menu.menu_filter, menu)
-        return super.onCreateOptionsMenu(menu)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when(item.itemId){
-            R.id.menu_filter ->{
-                list.add("2020")
-                list.add("2010")
-                list.add("2020")
-                list.add("2030")
-                list.add("2040")
-                loadFragmentChip(FilterFragment.newInstance(list as ArrayList<String>))
-            }
-        }
-        return super.onOptionsItemSelected(item)
     }
 
     //load fragment
